d7fe147980bd41af9e13f95bddf363a964676c96
viniciusdutra314 <dutravinisousa2@usp.br>
2023-09-09 18:19:10 -0300

    Histograma com coeficientes

--- Fourier/Series/series_fourier_gif.py
+++ Fourier/Series/series_fourier_gif.py
@@ -1,10 +1,9 @@
 import scipy.integrate as integrate
-import scipy.signal as signal
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import PillowWriter
 def function(t):
-    return t**3+np.exp(t)
+    return np.sin(t**2)+t
 def fourier_function(t, a_coefficients, b_coefficients, L):
     value=a_coefficients[0]/2
     for n in range(1, len(a_coefficients)):
@@ -24,13 +23,22 @@ N_terms = 100
 L = 3
 t = np.linspace(-L, L, 1000)
 writer=PillowWriter(fps=5)
-fig ,axis= plt.subplots()
+fig ,axis= plt.subplots(ncols=3,nrows=1,figsize=(15,5))
 with writer.saving(fig,'fourier_series.gif',100):
+    a_coefficients, b_coefficients=fourier_coefficients(function,N_terms=N_terms,L=L)
     for N in range(1,N_terms):
-        a_coefficients, b_coefficients=fourier_coefficients(function,N_terms=N,L=L)
-        y =np.array([fourier_function(tempo, a_coefficients, b_coefficients, L) for tempo in t])
-        axis.plot(t,y,label=f"Fourier series N={N}")
-        axis.plot(t,function(t),label=f"Function",alpha=0.5)
-        axis.legend()
+        y =np.array([fourier_function(tempo, a_coefficients[:N], b_coefficients[:N], L) for tempo in t])
+        fig.suptitle(r"Série de fourier da função $y(t)=sin(t**2)+t")
+        axis[0].plot(t,y,label=f"Fourier series N={N}")
+        axis[0].plot(t,function(t),label=f"Function",alpha=0.5)
+        axis[0].set_xlabel("tempo")
+        axis[0].set_ylabel(r"f(tempo)")
+        axis[1].plot(range(len(a_coefficients)),a_coefficients,label=r"$a_n$",color='g')
+        axis[1].set_xlabel("Nº termo")
+        axis[2].plot(range(len(b_coefficients)),b_coefficients,label=r"$b_n$",color='r')
+        axis[2].set_xlabel("Nº termo")
+        for ax in axis:
+            ax.grid()
+            ax.legend()
         writer.grab_frame()
-        axis.clear()
+        for ax in axis:ax.clear()
